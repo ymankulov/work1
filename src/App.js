996147d0aa14193d90ef21865daa5cdc7b999c51
ymankulov <ymankulov253546@gmail.com>
2022-07-06 14:47:11 -0800

    asdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "../src/components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
      <Header/>
+        <Footer/>
 
     </div>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            sm,dafn
+            sm,dafn
+            sm,dafn
+            sm,dafn
+        </div>
+    );
+};
+
+export default Footer;
