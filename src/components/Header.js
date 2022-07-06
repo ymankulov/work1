827c60948bbe257a210e9fc64bed32a6b716960c
ymankulov <ymankulov253546@gmail.com>
2022-07-06 14:43:18 -0800

    asdfg

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                kmhbf
+            </div>
+            <div>
+                kmhbf
+            </div><div>
+                kmhbf
+            </div><div>
+                kmhbf
+            </div>
+        </div>
+    );
+};
+
+export default Header;
